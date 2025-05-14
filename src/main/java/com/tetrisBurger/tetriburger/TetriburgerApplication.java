79a7b62d9dc7f686131651a6ce7d9c9324785280
Loaiza <andresll1205@gmail.com>
2025-05-13 19:05:04 -0500

    Primer commit del proyecto TetriBurger

---
+++ src/main/java/com/tetrisBurger/tetriburger/TetriburgerApplication.java
@@ -0,0 +1,13 @@
+package com.tetrisBurger.tetriburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetriburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetriburgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tetrisBurger/tetriburger/TetriburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetrisBurger.tetriburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetriburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
